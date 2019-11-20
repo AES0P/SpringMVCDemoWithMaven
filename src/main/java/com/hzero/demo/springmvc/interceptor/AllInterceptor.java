403f98719450ee4969102c121046882b4b3bda86
Aesop <wentao.tian@hand-china.com>
2019-11-20 11:15:48 +0800

    Remove unnecessary library By Aesop 2019.11.20

--- src/main/java/com/hzero/demo/springmvc/interceptor/AllInterceptor.java
+++ src/main/java/com/hzero/demo/springmvc/interceptor/AllInterceptor.java
@@ -18,7 +18,7 @@ public class AllInterceptor implements WebRequestInterceptor {
         System.out.println("for ALL controller preHandle(), 在访问Controller之前被调用");
         request.setAttribute("request", "request", WebRequest.SCOPE_REQUEST);//这个是放到request范围内的，所以只能在当前请求中的request中获取到
         request.setAttribute("session", "session", WebRequest.SCOPE_SESSION);//这个是放到session范围内的，如果环境允许的话它只能在局部的隔离的会话中访问，否则就是在普通的当前会话中可以访问
-        request.setAttribute("globalSession", "globalSession", WebRequest.SCOPE_GLOBAL_SESSION);//如果环境允许的话，它能在全局共享的会话中访问，否则就是在普通的当前会话中访问
+//        request.setAttribute("globalSession", "globalSession", WebRequest.SCOPE_GLOBAL_SESSION);//如果环境允许的话，它能在全局共享的会话中访问，否则就是在普通的当前会话中访问
     }
 
     /**
